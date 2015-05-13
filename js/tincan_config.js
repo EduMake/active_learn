d504d39d328893dfe8b0cbb92d917198114d1094
Martyn Eggleton <martyn.eggleton@gmail.com>
2015-05-13 12:00:59 +0100

    Atempt at #1 - removing tincan

--- js/tincan_config.js
+++ js/tincan_config.js
@@ -1,1 +1,8 @@
-var TinCanRecordStores = [];
+var TinCanRecordStores = [
+    {
+        endpoint: "http://lrs.edumake.org/data/xAPI/",
+        username: "77d48e666c68b18b8817bcdfbec4363d3571730b",
+        password: "a26c0ce065135e5ac08fc2a3161546bf069c282f",
+        allowFail: false
+    }
+];
